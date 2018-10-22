f4b4a6cc4d430654fe1a709cf7d74c4f7749f137
Uilian Ries <uilianries@gmail.com>
2018-10-22 18:17:43 -0300

    Apply conan conventions
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- conanfile.py
+++ conanfile.py
@@ -9,21 +9,21 @@ class DoctestConan(ConanFile):
     name = "doctest"
     version = "2.0.0"
     url = "https://github.com/bincrafters/conan-doctest"
+    homepage = "https://github.com/onqtam/doctest"
+    author = "Bincrafters <bincrafters@gmail.com>"
     description = "C++98/C++11 single header testing framework"
     license = "MIT"
     exports = ["LICENSE.md"]
-    source_subfolder = "source_subfolder"
+    _source_subfolder = "source_subfolder"
     
     def source(self):
-        source_url = "https://github.com/onqtam/doctest"
-        tools.get("{0}/archive/{1}.tar.gz".format(source_url, self.version))
+        tools.get("{0}/archive/{1}.tar.gz".format(self.homepage, self.version))
         extracted_dir = self.name + "-" + self.version
-        os.rename(extracted_dir, self.source_subfolder)
-        #Rename to self.source_subfolder is a convention to simplify later steps
+        os.rename(extracted_dir, self._source_subfolder)
 
     def package(self):
-        self.copy(pattern="LICENSE.txt", dst="licenses", src=self.source_subfolder)
-        self.copy(pattern="*doctest.h*", dst="include", src=os.path.join(self.source_subfolder,"doctest"))
+        self.copy(pattern="LICENSE.txt", dst="licenses", src=self._source_subfolder)
+        self.copy(pattern="*doctest.h*", dst="include", src=os.path.join(self._source_subfolder,"doctest"))
 
     def package_id(self):
         self.info.header_only()
