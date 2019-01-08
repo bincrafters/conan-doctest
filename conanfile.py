75be12424f1c5cd36421cedffc3a1137bf30988e
chamatht <chamatht@gmail.com>
2019-01-08 13:22:24 +0100

    Updated to 2.2.0

--- conanfile.py
+++ conanfile.py
@@ -7,7 +7,7 @@ import os
 
 class DoctestConan(ConanFile):
     name = "doctest"
-    version = "2.0.1"
+    version = "2.2.0"
     url = "https://github.com/bincrafters/conan-doctest"
     homepage = "https://github.com/onqtam/doctest"
     author = "Bincrafters <bincrafters@gmail.com>"
